4c7cf2fb905aaec75d2cacbac36d9eb3636782bc
07shrutik <shrutikmahajan1414@gmail.com>
2023-04-26 21:38:04 +0530

    twitter validation

--- twitterclone/src/App.jsx
+++ twitterclone/src/App.jsx
@@ -1,21 +1,23 @@
 
 import './App.css'
-// import { Login } from './components/Login'
+import Login  from './pages/Login'
+// import Register from './pages/Register'
 // import { Register } from './components/Register'
 // import { Account } from './components/create account/Account'
-import { Step_2 } from './components/create account/Step2'
-import { Step_3 } from './components/create account/Step3'
+// import { Step_2 } from './components/create account/Step2'
+// import { Step_3 } from './components/create account/Step3'
 
 function App() {
   
 
   return (
     <>
-     {/* <Login/> */}
+     <Login />
+
      {/* <Register/> */}
      {/* <Account/> */}
      {/* <Step_2/> */}
-     <Step_3/>
+     {/* <Step_3/> */}
     </>
   )
 }
--- twitterclone/src/main.jsx
+++ twitterclone/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/login" element={<App/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+   
   </React.StrictMode>,
 )
